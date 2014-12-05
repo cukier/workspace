9c62fd647adb087de71220434926fd25c5672ca2
cuki <mauriciocukier@gmail.com>
2014-12-05 16:59:01 -0200

    Serial

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/f4/9083b174867c00141f9ef01d0584075c
@@ -0,0 +1,3 @@
+main.d: ../main.c ../rs232.h
+
+../rs232.h:
---
+++ Serial/src/serial.c
@@ -0,0 +1,84 @@
+/*
+ * serial.c
+ *
+ *  Created on: 05/12/2014
+ *      Author: cuki
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <termios.h>
+#include <stdlib.h>
+
+int open_port(char *porta) {
+	int fd;
+
+	fd = open(porta, O_RDWR | O_NOCTTY | O_NDELAY);
+
+	if (fd == -1) {
+		fprintf(stderr, "Problemas ao abrir a porta \"%s\" : %s\n", porta,
+				strerror(errno));
+		exit(-1);
+	}
+
+	return fd;
+}
+
+int set_port(int baud_rate, int fd) {
+	struct termios options;
+	speed_t speed;
+
+	switch (baud_rate) {
+	default:
+	case 9600:
+		speed = B9600;
+		break;
+	case 19200:
+		speed = B19200;
+		break;
+	}
+
+	tcgetattr(fd, &options);
+	cfsetispeed(&options, speed);
+	cfsetospeed(&options, speed);
+	options.c_cflag |= (CLOCAL | CREAD);
+	tcsetattr(fd, TCSANOW, &options);
+
+	return 0;
+}
+
+int get_msg_size(char *msg) {
+	int cont;
+	for (cont = 0; *msg != '\0'; msg++, cont++)
+		;
+
+	return cont;
+}
+
+int main(int argc, char **argv) {
+
+	int fd, n, s;
+	char str[160];
+
+	fd = open_port("/dev/ttyS0");
+
+	set_port(9600, fd);
+
+	strcpy(str, "\fHelloDude");
+
+	s = get_msg_size(str);
+
+	printf("Tamanho da mensagem %d", s);
+
+	n = write(fd, str, s);
+
+	printf("\nEnviados %d bytes", n);
+
+	close(fd);
+
+	return 0;
+
+}
